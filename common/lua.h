86f8b9d528bda306e4a1015dabcab4f74dc7cb85
murphycui <1224066169@qq.com>
2023-04-04 16:46:57 +0800

    基础数据结构更新

--- common/lua.h
+++ common/lua.h
@@ -1,6 +1,14 @@
 #ifndef lua_h
 #define lua_h
 
+#if defined(LLONG_MAX)
+#define LUA_INTEGER long
+#define LUA_NUMBER double
+#else
+#define LUA_INTEGER int 
+#define LUA_NUMBER float 
+#endif 
+
 // 基础类型
 #define LUA_TNUMBER 1
 #define LUA_TLIGHTUSERDATA 2
@@ -12,7 +20,5 @@
 #define LUA_TTHREAD 8
 #define LUA_TNONE 9
 
-#define LUA_INTEGER long
-#define LUA_NUMBER double
 
 #endif
--- common/luaobject.h
+++ common/luaobject.h
@@ -7,8 +7,30 @@ typedef struct lua_State lua_State;
 
 typedef LUA_INTEGER lua_Integer;
 typedef LUA_NUMBER lua_Number;
-typedef unsigned char lua_Byte;
-typedef int (*lua_CFunction) (lua_State* L); // light c function只有一个int类型的返回值
-                                            // 代表调用完成后还有多少个返回值在栈中
+typedef unsigned char lu_byte;
+typedef int (*lua_CFunction)(lua_State* L); // 定义light c function函数指针
+
+typedef union lua_Value {
+    void* p; // 存放light userdata
+    int b;
+    lua_Integer i;
+    lua_CFunction f;
+} Value;
+
+typedef struct lua_TValue {
+    Value value_;  // 值
+    int tt_;       // 类型信息
+} TValue;
+
+
+#define LUA_NUMINT (LUA_TNUMBER | (0 << 4)) // int number
+#define LUA_NUMFLT (LUA_TNUMBER | (1 << 4)) // float number
+
+#define LUA_TLCL (LUA_TFUNCTION | (0 << 4)) // lua closure
+#define LUA_TLCF (LUA_TFUNCTION | (1 << 4)) // light c function
+#define LUA_TCCL (LUA_TFUNCTION | (2 << 4)) // c closure
+
+#define LUA_LNGSTR (LUA_TSTRING | (0 << 4)) // long string
+#define LUA_SHRSTR (LUA_TSTRING | (1 << 4)) // short string
 
 #endif
